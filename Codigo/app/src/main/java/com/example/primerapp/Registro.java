c01473dc41ccbb401ea436139123e9e246876fc1
Fernando Nicolas Sanchez <sanchez36822171@gmail.com>
2021-06-09 16:52:05 -0300

    http optimizado

--- Codigo/app/src/main/java/com/example/primerapp/Registro.java
+++ Codigo/app/src/main/java/com/example/primerapp/Registro.java
@@ -3,9 +3,13 @@ package com.example.primerapp;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import org.json.JSONException;
 
@@ -18,15 +22,30 @@ import java.nio.charset.StandardCharsets;
 
 public class Registro extends AppCompatActivity {
 
+    TextView name;
+    TextView surname;
+    TextView email;
+    TextView pw;
+    TextView dni;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
     }
 
+    @SuppressLint("ResourceType")
     public void registrar (View view) throws IOException, JSONException {
 
-        new RequestAPIRest(getApplicationContext()).execute("Tarea ejecutada correctamente.");
+        RequestAPIRest hilo = new RequestAPIRest();
+
+        name = (TextView) findViewById(R.id.inNombre);
+        surname = (TextView) findViewById(R.id.inApellido);
+        dni = (TextView) findViewById(R.id.inDNI);
+        email = (TextView) findViewById(R.id.inCorreo);
+        pw = (TextView) findViewById(R.id.inPass);
+
+        hilo.execute(name.getText().toString(), surname.getText().toString(), dni.getText().toString(), email.getText().toString(), pw.getText().toString());
 
     }
 
--- Codigo/app/src/main/java/com/example/primerapp/RequestAPIRest.java
+++ Codigo/app/src/main/java/com/example/primerapp/RequestAPIRest.java
@@ -1,30 +1,21 @@
 package com.example.primerapp;
-import android.content.Context;
-import android.os.AsyncTask;
-import android.os.StrictMode;
-import android.widget.Toast;
-
-import android.content.Context;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class RequestAPIRest extends AsyncTask<String, Void, String> {
+public class RequestAPIRest extends android.os.AsyncTask<String, Void, Integer> {
 
-    private Context context;
-
-    public RequestAPIRest(Context context) {
-        this.context = context;
-    }
 
     @Override
-    protected String doInBackground(String... params) {
+    protected Integer doInBackground(String... params) {
         URL url = null;
+        System.out.println(params.toString());
         try {
             url = new URL("http://so-unlam.net.ar/api/api/register");
         } catch (MalformedURLException e) {
@@ -36,19 +27,28 @@ public class RequestAPIRest extends AsyncTask<String, Void, String> {
             conn.setRequestMethod("POST");
             conn.setRequestProperty("Content-Type", "application/json");
             conn.setDoOutput(true);
+            conn.setDoInput(true);
+            conn.setConnectTimeout(5000);
+            System.out.println(params.length);
             JSONObject json = new JSONObject();
             json.put("env", "TEST");
-            json.put("name", "FERNANDO");
-            json.put("lastname", "SANCHEZ");
-            json.put("dni", new Integer(36822171));
-            json.put("email", "SANCHEZ@GMAIL.COM");
-            json.put("password", "ABCDFGHIJK");
+            json.put("name", params[0]);
+            json.put("lastname", params[1]);
+            json.put("dni", params[2]);
+            json.put("email", params[3]);
+            json.put("password", params[4]);
             json.put("commission", new Integer(2900));
             json.put("group", new Integer(11));
             System.out.println(json);
-            byte[] postDataBytes = json.toString().getBytes("UTF-8");
-            conn.getOutputStream().write(postDataBytes);
-            System.out.println(conn.getResponseCode() + " " + conn.getResponseMessage());
+            OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+            wr.write(json.toString());
+            wr.flush();
+            conn.connect();
+            int resp = conn.getResponseCode();
+            if(resp == 200) {
+                System.out.println(resp + " " + conn.getResponseMessage());
+                return resp;
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -59,9 +59,9 @@ public class RequestAPIRest extends AsyncTask<String, Void, String> {
         return null;
     }
 
-    @Override
-    protected void onPostExecute(String result) {
-        Toast.makeText(context, result,
-                Toast.LENGTH_LONG).show();
+    protected int onPostExecute(int result){
+        return result;
     }
+
+
 }
