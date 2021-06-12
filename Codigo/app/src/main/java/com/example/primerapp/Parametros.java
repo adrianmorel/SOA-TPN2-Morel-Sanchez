59cf72a9d6d43225c613f051e3953015675c64e3
Adrián Morel <emer6iendo@gmail.com>
2021-06-12 19:27:12 -0300

    Agregados en activity parametros

--- Codigo/app/src/main/java/com/example/primerapp/Parametros.java
+++ Codigo/app/src/main/java/com/example/primerapp/Parametros.java
@@ -1,30 +1,51 @@
 package com.example.primerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
+import android.widget.Toast;
+import java.util.ArrayList;
 import java.util.List;
+import static android.widget.ArrayAdapter.*;
 
 public class Parametros extends AppCompatActivity {
 
-    private Spinner opciones;
+    private Spinner cantPersonas = findViewById(R.id.comboPersonas);
+    private Spinner mascarilla;
+    private Spinner ventana;
+    private Spinner tiempoHablando;
+    private Spinner volumenVoz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parametros);
 
-        opciones = findViewById(R.id.listOpciones);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.personas, android.R.layout.simple_spinner_item);
-        opciones.setAdapter(adapter);
-    }
+        cantPersonas = findViewById(R.id.comboPersonas);
+        ArrayAdapter<CharSequence> adapterPersonas = ArrayAdapter.createFromResource(this, R.array.personas, android.R.layout.simple_spinner_item);
+        cantPersonas.setAdapter(adapterPersonas);
 
-    public void confirmarParametros(View view){
+        mascarilla = findViewById(R.id.comboMascarillas);
+        ArrayAdapter<CharSequence> adapterMascarilla = ArrayAdapter.createFromResource(this,R.array.mascarillas, android.R.layout.simple_spinner_item);
+        mascarilla.setAdapter(adapterMascarilla);
 
-    }
+        ventana = findViewById(R.id.comboVentanas);
+        ArrayAdapter<CharSequence> adapterVentana = ArrayAdapter.createFromResource(this, R.array.ventanas, android.R.layout.simple_spinner_item);
+        ventana.setAdapter(adapterVentana);
+
+        tiempoHablando = findViewById(R.id.comboTiempoHablando);
+        ArrayAdapter<CharSequence> adapterTiempo = ArrayAdapter.createFromResource(this,R.array.tiempoHablando, android.R.layout.simple_spinner_item);
+        tiempoHablando.setAdapter(adapterTiempo);
+
+        volumenVoz = findViewById(R.id.comboVolumenDeVoz);
+        ArrayAdapter<CharSequence> adapterVolumen = ArrayAdapter.createFromResource(this,R.array.volumenDeVoz, android.R.layout.simple_spinner_item);
+        volumenVoz.setAdapter(adapterVolumen);
+
+        }
+
 }
